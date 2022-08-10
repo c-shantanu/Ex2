ba1b68f6ebe8e8998f9493731701c5e25743a17d
c-shantanu <itsmeshantranuchauhan@gmail.com>
2022-08-10 10:27:53 +0200

    Ex2

--- exercise.py
+++ exercise.py
@@ -27,9 +27,11 @@ c = a*b
 print(c,a*a,b*b)
 
 a = 40
-b = 15
+b = 15.5
 c = a/b
-print(c)
+d = a//b
+print(c,d)
+print(round(c))
 
 a = 29
 b = 6
@@ -45,3 +47,46 @@ z = 4+5j
 a = 2
 b = a*z
 print(b)
+
+
+### Task 2 - basic math functions
+
+numbers = [9, 34, 11, -4, 27]
+
+# find the maximum number
+max_number = max(numbers)
+print(max_number)
+
+# find the smallest number
+min_number = min(numbers)
+print(min_number)
+
+
+number = -20
+absolute_number = abs(number)
+print(absolute_number)
+
+# returns 2^2
+print(pow(2, 2))  
+
+# returns -2^2
+print(pow(-2, 2))    
+
+# returns 1/2^2 
+print(pow(2, -2))   
+ 
+# returns -1/-2^2 
+print(pow(-2, -2))
+
+
+
+# print using floor() method
+print ("math.floor(-23.11) : ", math.floor(-23.11))
+print ("math.floor(300.16) : ", math.floor(300.16))
+print ("math.floor(300.72) : ", math.floor(300.72))
+
+
+# print ceil() method
+print ("math.ceil(-23.11) : ", math.ceil(-23.11))
+print ("math.ceil(300.16) : ", math.ceil(300.16))
+print ("math.ceil(300.72) : ", math.ceil(300.72))
