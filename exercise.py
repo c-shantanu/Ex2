2ab3d0733bf1a79fb1a8e12fcc7738a9b808c9b0
c-shantanu <itsmeshantranuchauhan@gmail.com>
2022-08-10 09:45:06 +0200

    Ex2

---
+++ exercise.py
@@ -0,0 +1,47 @@
+# do exercise here
+# upload to github for portfolio
+# thank me later when your git graph is as green as the python money u gonna earn
+
+### Task 1 - basic math operations
+
+a = 65
+b = 26
+c=a+b
+
+print(c)
+
+a = 65.25
+b = 28.4
+c = a+b
+
+print(c)
+
+a = 56
+b = 98
+c = a-b
+print(c)
+
+a = 5
+b = 9
+c = a*b
+print(c,a*a,b*b)
+
+a = 40
+b = 15
+c = a/b
+print(c)
+
+a = 29
+b = 6
+c = a%b
+print(c)
+
+a = True
+b = False
+c = a*b
+print(c,b*b,a*a,b*a)
+
+z = 4+5j
+a = 2
+b = a*z
+print(b)
